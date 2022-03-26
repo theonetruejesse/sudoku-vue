eaf1d705121ae3aa7cdcad747dc7c38e01769239
Jesse Lee <jesselee@Jesses-MacBook-Air.local>
2022-03-26 11:22:42 -0700

    added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  purge: { content: ["./public/**/*.html", "./src/**/*.vue"] },
+  content: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
